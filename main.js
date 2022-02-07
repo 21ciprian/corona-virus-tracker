b9e0acc0d501deb019f5edaa3c15270c78ea9e63
21ciprian <2021ciprianoffice@gmail.com>
2022-02-07 23:40:53 +0000

    Declared variables

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log('linked')
+const selectCountries = document.querySelector('#select')
+const todayCases = document.querySelector('#todayCases')
+const totalCases = document.querySelector('#totalCases')
+const todayRecovered = document.querySelector('#todayRecovered')
+const totalRecovered = document.querySelector('#totalRecovered')
+const todayDeaths = document.querySelector('#todayDeaths')
+const totalDeaths = document.querySelector('#totalDeaths')
+const covidCases = document.querySelector('#covidCases')
+const covidRecoveries = document.querySelector('#covidRecoveries')
+const covidDeaths = document.querySelector('#covidDeaths')
+const tableCases = document.querySelector('#table')
+const ctx = document.querySelector('#myChart')
+let chartCountryCode = 'worldwide'
+
+selectCountries.addEventListener('click', () => console.log('clicked select'))
