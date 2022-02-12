54d080cc2b3f098bd09900a1eb50b073d87fdf61
21ciprian <2021ciprianoffice@gmail.com>
2022-02-12 22:29:58 +0000

    Created function to fetch all death cases

--- main.js
+++ main.js
@@ -59,3 +59,22 @@ async function fetchAllRecoveredCases() {
 	populateTable(countries)
 	// console.log('countries fetched: ', data)
 }
+/fetch all death cases
+async function fetchAllDeathCases() {
+	const response = await fetch(`https://disease.sh/v3/covid-19/countries`)
+	const data = await response.json()
+
+	// loop over data and get the country name and country iso2 "uk, us ..."
+	const countries = data.map(function (country) {
+		return {
+			name: country.country,
+			value: country.countryInfo.iso2,
+			cases: country.cases,
+			flag: country.countryInfo.flag,
+		}
+	})
+	showOnMap(data, 'deaths')
+	populateOptions(countries)
+	populateTable(countries)
+	// console.log('countries fetched: ', data[0].countryInfo.flag)
+}
